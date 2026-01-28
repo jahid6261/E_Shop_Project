9b29209e50a26ec7b3fb49c11ff8f6ee0ef842d7
Jahid Alam <jahidalam01741@gmail.com>
2026-01-28 10:08:37 -0800

    Add STATIC_ROOT and whitenoise for production

--- e_shop/settings.py
+++ e_shop/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,11 +136,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-
+STATICFILES_DIRS = [BASE_DIR / "static"]   
+STATIC_ROOT = BASE_DIR / "staticfiles" 
 
 MEDIA_URL= '/media/'
 MEDIA_ROOT='media'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Login_URL
