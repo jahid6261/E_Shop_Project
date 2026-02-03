9180e5662c1ee12fe165b7e06a23af939a830734
Jahid Alam <jahidalam01741@gmail.com>
2026-02-02 20:38:36 -0800

    ready for deployment

--- shop/admin.py
+++ shop/admin.py
@@ -1,18 +1,20 @@
 from django.contrib import admin
-from shop.models import Category,Product,Rating
-# Register your models here.
+from shop.models import Category, Product, Rating, ProductImage
+
+# Category
 admin.site.register(Category)
 
+# Rating
+admin.site.register(Rating)
 
+# ProductImage Inline
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1  
 
-
+# Product Admin
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display=['name','slug','price','stock','available','created_at','updated_at']
+    list_display = ['name', 'slug', 'price', 'stock', 'available', 'created_at', 'updated_at']
     prepopulated_fields = {'slug': ('name',)}
-
-
-
-
-
-admin.site.register(Rating)
+    inlines = [ProductImageInline]  
